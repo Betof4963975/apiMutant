ac4a813ef4dd63f6a1a322d2186f3a30bc63f54d
Betof4963975 <58694849+Betof4963975@users.noreply.github.com>
2019-12-10 15:04:06 -0300

    Update findMutants.php

--- api/objects/findMutants.php
+++ api/objects/findMutants.php
@@ -11,128 +11,62 @@ class findMutants{
 
 	function esMutante()
 	{
-		$adnIngresado = $this->adn;
-		$coincidenciasFila = 0;
-		$coincidenciasCol = 0;
-		$coincidenciasDiag = 0;
-		$encontradas = 0;
-		$encontradasLinea = 0;
-		$encontradasDiag = 0;
-		$encontradasCol = 0;
-		$esBueno = false;
-		//obtengo el array desde el string adn
-		$arrayCadenaAdn = explode(",", $this->adn);
-		//creo una matriz con cada elemento del array adn
+		$arrayCaracteres = array("A","T","G","C");
+		$array2 = $_POST['dna'];
+		$array2=explode(",",$array2);
 		$arrayMatriz = array();
-		//intento de armado de matriz
-		for($i=0; $i < sizeof($arrayCadenaAdn); $i++) {
-			$fila = str_split($arrayCadenaAdn[$i]);
+		for($i=0; $i < sizeof($array2); $i++) 
+		{
+			$fila = str_split($array2[$i]);
 			array_push($arrayMatriz, $fila);
 		}
-		//var_dump($arrayMatriz);
-		for ($i=0; $i < sizeof($arrayMatriz); $i++) {
-
-			$encontradasLinea = $this->buscarlinea($i,$arrayMatriz);
-			$encontradasCol =  $this->buscarColumna($i,$arrayMatriz);
-			$encontradasDiag =  $this->buscarDiagonal($i,$arrayMatriz);
-		}
-		echo "lineas encontradas=".$encontradasLinea."<br>";
-		echo "columnas encontradas=".$encontradasCol."<br>";
-		echo "diagonales encontradas=".$encontradasDiag."<br>";
-		$total = $encontradasLinea + $encontradasCol + $encontradasDiag;
-		if ($total >= 2) {
-			$esBueno = true;
-		}
-		echo "resultado=".$esBueno;
-		// ---------------------------------------
-		echo "Cadena a guardar Primero =".$adnIngresado."<br>";
-		$this->guardarCadena($adnIngresado,$esBueno);
-		//echo "Cantidad de coincidencias:".$encontradas."<br>";
-		if ($esBueno == true) {
-			return 1;
-		}
-		else{
-			return 0;
-		}
-	}
-
-
-	function buscarlinea($indice,$matriz)
-	{	
-		$coincidencias = 0;
-		$encontradasFila = 0;
-		$encontradas = 0;
-		$elemento = $matriz[$indice][$indice];
-		//echo "Valor indicieFila:".$indice;
-		//echo "Elemento Indice Fila:".$elemento."<br>";
-		for ($j=0; $j <count($matriz[$indice],1) ; $j++) {		
-			$comparar = $matriz[$indice][$j];
-			//echo "Valor J:".$j;
-			//echo "elemento fila:".$comparar."<br>";
-			if ($matriz[$indice][$indice] == $matriz[$indice][$j])
+		for($p=0;$p < sizeof($arrayCaracteres); $p++){
+			$palabra_a_buscar = $arrayCaracteres[$p].$arrayCaracteres[$p].$arrayCaracteres[$p].$arrayCaracteres[$p];
+			echo "palabra:".$palabra_a_buscar."<br>";
+			if (strlen($palabra_a_buscar) > 0) 
 			{
-				
-				$coincidencias = $coincidencias + 1;
+				for ($i=0; $i < sizeof($arrayMatriz); $i++) 
+				{
+					for ($j=0; $j < sizeof($arrayMatriz[$i]); $j++)
+					{
+						if ($arrayMatriz[$i][$j] === $palabra_a_buscar[0])
+						{
+							recursividad($i, $j, 0, $arrayMatriz, $palabra_a_buscar);
+						}
+					}
+				}
 			}
-			else{
-				break;
+		}		
+	}
+	function recursividad($indice_i, $indice_j, $posicion, $array, $palabra_a_buscar) 
+	{
+			$posicion = $posicion+1;
+			for ($i=$indice_i-1; $i < $indice_i+2; $i++) 
+			{
+				for ($j=$indice_j-1; $j < $indice_j+2; $j++) 
+				{
+					if (($i !== $indice_i) || ($j !== $indice_j)) 
+					{
+						if ($array[$i][$j] === $palabra_a_buscar[$posicion]) 
+						{
+							if ($posicion < strlen($palabra_a_buscar)) 
+							{
+								recursividad($i, $j, $posicion, $array, $palabra_a_buscar);
+							} 
+							else 
+							{
+								echo "La palabra existe";
+							}
+						}
+					}
+				}
 			}
-		}
-		if ($coincidencias >= 4) {
-			$encontradas =  1;
-		}
-		return $encontradas;
 	}
 
-	function buscarColumna($indice,$matriz)
-	{
-		$coincidencias = 0;
-		$encontradasColumna = 0;
-		$j = $indice;
-		$elemento = $matriz[$indice][$indice];
-		//echo "Valor indicieCol:".$indice;
-		//echo "Elemento Indice Col:".$elemento."<br>";
-		for ($i=0; $i <sizeof($matriz[$indice]) ; $i++) {
-			//var_dump($matriz[$indice][$indice]);
-			//echo "Valor I:".$i;
-			$comparar = $matriz[$i][$indice];
-			//echo "elemnto Col:".$comparar."<br>";
-			if ($matriz[$indice][$indice] == $matriz[$i][$j]) {
-				$coincidencias = $coincidencias + 1;
-			}
-			else{
-				break;
-			}
-		}
-		if ($coincidencias >= 4) {
-			$encontradasColumna =  1;
-		}
-		return $encontradasColumna;
-	}
+	
 
-	function buscarDiagonal($indice,$matriz)
-	{
-		$coincidencias = 0;
-		$encontradasDiagonal = 0;
-		$j = $indice;
-		for ($i=0; $i <sizeof($matriz[$indice]) ; $i++) {
-			echo "Valor I:".$i;
-			$comparar = $matriz[$i][$i];
-			echo "elemnto Diag:".$comparar."<br>";
-			if ($matriz[$indice][$indice] == $matriz[$i][$i]) {
-				$coincidencias = $coincidencias + 1;
-				echo "coincidencias Diag=".$coincidencias."<br>";
-			}
-			else{
-				break;
-			}
-		}
-		if ($coincidencias >= 4) {
-			$encontradasDiagonal = 1;		
-		}
-		echo "encontradas Diag=".$encontradasDiagonal."<br>";
-		return $encontradasDiagonal;
-	}
+	
+	
 
 	function guardarCadena($adn,$mutante)
 	{
