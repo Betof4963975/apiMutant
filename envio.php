0d9c336cef8dff660c2f77dbe98e1b3d89941261
Betof4963975 <58694849+Betof4963975@users.noreply.github.com>
2019-12-10 12:31:23 -0300

    Create envio.php

---
+++ envio.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Valores</title>
+</head>
+<body>
+<form method="POST" action="sopa.php" >	
+	<input type="text" name="dna" value="ATGCGA">
+	<input type="submit">
+</form>
+</body>
+</html>
