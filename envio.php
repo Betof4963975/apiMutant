b06387a4ba88433b5169e2f9062d18e320c2891a
Betof4963975 <58694849+Betof4963975@users.noreply.github.com>
2019-12-10 12:34:55 -0300

    Update envio.php

--- envio.php
+++ envio.php
@@ -4,8 +4,8 @@
 	<title>Valores</title>
 </head>
 <body>
-<form method="POST" action="sopa.php" >	
-	<input type="text" name="dna" value="ATGCGA">
+<form method="POST" action="mutant.php" >	
+	<input type="text" name="dna" value="">
 	<input type="submit">
 </form>
 </body>
