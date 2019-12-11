8930c37e7e05f5d267e4e0baaeeb0cbf6fd5dd42
Betof4963975 <58694849+Betof4963975@users.noreply.github.com>
2019-12-10 21:53:44 -0300

    Add files via upload

--- envio.php
+++ envio.php
@@ -4,7 +4,7 @@
 	<title>Valores</title>
 </head>
 <body>
-<form method="POST" action="mutant.php" >	
+<form method="POST" action="../api/mutant/mutant.php" >		
 	<input type="text" name="dna" value="">
 	<input type="submit">
 </form>
