f68ea790ce7c0a8212f791d14e21be4197abc551
Betof4963975 <58694849+Betof4963975@users.noreply.github.com>
2019-12-26 12:28:56 -0300

    Update envio.php

--- envio.php
+++ envio.php
@@ -8,5 +8,8 @@
 	<input type="text" name="dna" value="">
 	<input type="submit">
 </form>
+<form method="POST" action="../api/mutant/stats.php" >
+	<input type="submit" value="Estadisticas">
+</form>
 </body>
 </html>
